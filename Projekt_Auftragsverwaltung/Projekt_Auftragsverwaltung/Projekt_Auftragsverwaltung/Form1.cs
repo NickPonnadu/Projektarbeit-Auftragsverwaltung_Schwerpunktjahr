16b97b7c863acbb1f129397f27557bf7bb4d9b2a
NickPonnadu <96075309+NickPonnadu@users.noreply.github.com>
2023-01-03 19:54:42 +0100

    Gui_1

--- Projekt_Auftragsverwaltung/Projekt_Auftragsverwaltung/Projekt_Auftragsverwaltung/Form1.cs
+++ Projekt_Auftragsverwaltung/Projekt_Auftragsverwaltung/Projekt_Auftragsverwaltung/Form1.cs
@@ -6,5 +6,20 @@ namespace Projekt_Auftragsverwaltung
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked_1(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
     }
 }
